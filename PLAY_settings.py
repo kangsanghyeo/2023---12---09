d08fbe690e097c2488b5657d468f9393c589f39d
kangsanghyeo <120238777+kangsanghyeo@users.noreply.github.com>
2023-12-09 17:44:54 +0900

    Add files via upload

---
+++ PLAY_settings.py
@@ -0,0 +1,14 @@
+TITLE = "Going up"
+WIDTH = 800
+HEIGHT = 700
+FPS = 60
+
+PLAYER_ACC = 1.5
+PLAYER_FRICTION = -0.2
+PLAYER_GRAVITY = 0.8
+
+PLATFORM_LIST = [(0, HEIGHT - 50, WIDTH, 50),
+                 (WIDTH/2 - 50, HEIGHT*3/5, 100, 20),
+                 (110, HEIGHT - 350, 100, 20),
+                 (350, 200, 100, 20),
+                 (175, 100, 50, 50)]
